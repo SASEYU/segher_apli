7dbc0138bdb50fd297f0d318d77c98f87b998478
Samuel <samuelsegur@gmail.com>
2025-04-10 19:17:04 -0500

    actualizacion de archivo js

--- cotizacion.js
+++ cotizacion.js
@@ -1,18 +1,3 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-  
-    const usuario = document.getElementById("username").value;
-    const clave = document.getElementById("password").value;
-  
-    // Aquí puedes validar con tus datos reales o simular
-    if (usuario === "admin" && clave === "1234") {
-      alert("Login exitoso 🎉");
-      // Redirigir, guardar token, etc.
-    } else {
-      alert("Usuario o contraseña incorrectos ❌");
-    }
-  });
-  
 
 
 
@@ -53,7 +38,4 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
       }
     });
   });
-
-
-
 
